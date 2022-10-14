23ed8168d3e7b858c05b8a5d709e68d081654847
Дмитрий <90058654+DimaBazykin@users.noreply.github.com>
2022-10-14 14:13:08 +0300

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const {
+  override,
+  addWebpackAlias
+} = require("customize-cra");
+
+const path = require("path");
+
+module.exports = override(
+  addWebpackAlias({
+    "/src": path.resolve(
+      __dirname,
+      "src"
+    )
+  })
+);
